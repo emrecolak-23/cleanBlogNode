b2bf708901f43a05b12ba639d2e96b8a1743997e
Emre Çolak <colakkemre@gmail.com>
2022-02-25 16:28:19 +0300

    Create model and dynamic content

--- app.js
+++ app.js
@@ -1,15 +1,33 @@
 const express = require('express');
+const mongoose = require('mongoose')
+const Post = require('./models/Posts')
+
 
 const app = express();
-
-// Static File
-app.use(express.static('public'))
 
 // Template Engine
 app.set("view engine","ejs");
 
-app.get("/", (req,resp)=>{
-  resp.render('index');
+// Connect DB
+const dbURI = "mongodb+srv://emco:emco3232@nodetuts.iuulr.mongodb.net/cleanblog-test-db?retryWrites=true&w=majority"
+mongoose.connect(dbURI, {useNewUrlParser:true,useUnifiedTopology:true})
+.then((result)=>{ 
+  // declare port number
+  const PORT = 3000;
+  // listen for request
+  app.listen(PORT);} )
+.catch((err)=>{ console.log(err); })
+
+// Middleware
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/", async (req,resp)=>{
+  const posts = await Post.find();
+  resp.render('index', {
+    posts
+  });
 })
 
 app.get("/about", (req,resp) =>{
@@ -24,8 +42,11 @@ app.get("/add-post", (req,resp) =>{
   resp.render('add_post');
 })
 
-const PORT = 4000;
-
-app.listen(PORT, ()=>{
-  console.log(`Server created in ${PORT} port`)
+app.post("/post", async (req,resp) =>{
+  
+  await Post.create(req.body);
+  
+  resp.redirect('/');
 })
+
+
